c7b414d41fcf98b2c525a7b4d69e473dabe17b18
Jacobo <jacob.et.cetera@gmail.com>
2017-04-21 20:59:24 -0500

    Cambia codigo de Main -> Playground

--- Main.py
+++
@@ -1,8 +0,0 @@
-from Algoritmos.MM1 import mm1
-
-l = float(input("Ingresa lambda: "))
-m = float(input("Ingresa mu: "))
-n = float(input("Ingresa n: "))
-
-##print (str(type(l)) + " " +str(type(m)))
-print (mm1(l,m,n))
--- Playground.py
+++ Playground.py
@@ -1,1 +1,8 @@
-import numpy
+from Algoritmos.MM1 import mm1
+
+l = float(input("Ingresa lambda: "))
+m = float(input("Ingresa mu: "))
+n = float(input("Ingresa n: "))
+
+##print (str(type(l)) + " " +str(type(m)))
+print (mm1(l,m,n))
